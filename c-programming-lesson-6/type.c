4cde801b4cd082b28b6ef3263a6944981087b21f
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-10-05 01:08:53 +0300

    Lesson 6 finished

---
+++ c-programming-lesson-6/type.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+int main()
+{
+
+    int x = 3;
+    float y = 0.1;
+    printf("%f\n",x/y);
+
+    printf("%.4f\n",2*4.2 + 4%2);
+    printf("%.2f\n",12.5/2);
+    printf("%d\n",(int)3.2);
+    printf("%.6f\n",(float)5);
+    return 0;
+}

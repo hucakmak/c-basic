a7b9e6fa9386ed56df26227c9290c020037d84a0
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-10-04 23:50:38 +0300

    lesson 4 finished

---
+++ c-programming-lesson-4/aritmetik-2.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main()
+{
+    printf("%d\n",2+3-4);
+    printf("%d\n",11-3*3);
+    printf("%d\n",(5-2)/3);
+    printf("%d\n",2/3*4);
+    printf("%d\n",2*3/4);
+    printf("%d\n",(2+3)*4);
+    printf("%d\n",2+3*4);
+
+    return 0;
+}

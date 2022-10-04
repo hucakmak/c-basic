7143b553343666fb85fdfd4a024cfd66a96d73a9
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-10-04 23:36:39 +0300

    lesson 3 finished

---
+++ c-programming-lesson-3/aritmetik.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+int main() {
+
+
+    int x1 = 1;
+    int x2 = -1;
+
+    printf("%.0f\n",35.4+6.6);
+    printf("%d\n",21+21);
+    printf("%d\n",6*7);
+    printf("%d\n",42/1);
+    printf("%.0f\n",42.42-0.42);
+
+    return  0;
+}

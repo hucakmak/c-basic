68775917fdf6712a2d8f2dc23bba29b7b1305392
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-10-08 00:34:36 +0300

    Fibonacci for For finished

---
+++ c-programming-lesson-16/fibonaccifor.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+
+{
+
+    int ilksayi = 1;
+    int ikincisayi = 1;
+    int i;
+
+    printf("%d\n%d\n",ilksayi,ikincisayi);
+    for (i = 0; i < 12 ; i++)
+    {
+        int temp = ikincisayi;
+        ikincisayi += ilksayi;
+        ilksayi = temp;
+        printf("%d\n",ikincisayi);
+    }
+}

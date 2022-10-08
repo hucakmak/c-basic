25135726609ba9faecfce2d79ce63c5bb53ac5cf
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-10-08 21:30:52 +0300

    Bubble-sort learning

---
+++ c-programming-lesson-37/bubblesort.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+#define MAX 50
+
+void    bubblesort(int arr[],int size){
+
+
+    int i,j;
+    for (i = 0; i < size ; i++) {
+
+        for (j = 1; j <size - i ; j++)
+        {
+            if(arr[j-1] > arr[j])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+    }
+}
+int main(){
+
+    int array[MAX],size;
+    int i;
+    printf("Kaç elemanlı ? ");
+    scanf("%d",&size);
+
+    for (i = 0; i < size ; i++) {
+        scanf("%d",&array[i]);
+    }
+    bubblesort(array,size);
+
+    for(i=0; i < size;i++)
+    {
+        printf("%d ", array[i]);
+    }
+    return  0;
+}
